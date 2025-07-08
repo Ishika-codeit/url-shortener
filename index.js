3acc3c6ecc180d192a570d43dc67b6fcd919a100
Ishika-codeit <roji302005@gmail.com>
2025-07-08 21:48:06 +0530

    server side rendering

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express=require("express");
 const app=express();
 const port=2000;
-
+const path=require("path");
+const URL = require("./models/url");
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
@@ -13,7 +14,31 @@ connectmongo("mongodb://127.0.0.1:27017/short_url")
 })
 .catch((err) => console.log("error caught",err));
 
+
+
+// adding ui using ejs
+app.set("view engine","ejs");
+//fetching files from views
+app.set("views",path.resolve("./views"));
+
+
+// app.get("/test",async (req,res) =>{
+//     const allurls=await URL.find({});
+//    return res.render("home",{
+//     urls:allurls,
+//    });
+// })
+
+
+
+
+
+
+
+
 const urlrouter=require("./routes/url");
-app.use("/",urlrouter);
+const staticrouter=require("./routes/staticrouter");
+app.use("/url",urlrouter);
+app.use("/",staticrouter);
 
 app.listen(port,()=>console.log("server startedddddddddd"));
